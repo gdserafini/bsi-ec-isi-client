15cf0096a73c9dc56c39fc1128196953dc8969fd
Ana Cezanoski <juliacezanoski@gmail.com>
2024-05-22 18:29:07 -0300

    correção bd

--- src/database/ddl.sql
+++ src/database/ddl.sql
@@ -1,9 +1,13 @@
+-- Drop the database if it exists
 DROP DATABASE IF EXISTS green_path_db;
 
+-- Create the database
 CREATE DATABASE green_path_db DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
+-- Use the created database
 USE green_path_db;
 
+-- Create table for waste disposal locations
 CREATE TABLE LocalDescarte (
     id_local_descarte INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     endereco VARCHAR(255) NOT NULL,
@@ -14,16 +18,17 @@ CREATE TABLE LocalDescarte (
     horario_abertura TIME NOT NULL,
     horario_fechamento TIME NOT NULL,
     tipo VARCHAR(50) NOT NULL,
-    fk_Empresa_id INT
+    fk_Empresa_id INT NOT NULL
 );
 
+-- Create table for companies providing waste disposal services
 CREATE TABLE Empresa (
     id_empresa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome_fantasia VARCHAR(50) UNIQUE NOT NULL,
     avatar MEDIUMBLOB DEFAULT NULL,
     cnpj VARCHAR(50) UNIQUE NOT NULL,
-    razao_social VARCHAR(50) NOT NULL,
-    setor VARCHAR(50) NOT NULL,
+    razao_social VARCHAR(255) NOT NULL,
+    setor VARCHAR(255) NOT NULL,
     telefone VARCHAR(50) UNIQUE NOT NULL,
     bairro VARCHAR(50) NOT NULL,
     fk_Usuario_id INT NOT NULL,
@@ -31,14 +36,16 @@ CREATE TABLE Empresa (
     CONSTRAINT check_cnpj_digits CHECK(cnpj REGEXP '^[0-9]{14}$')
 );
 
+-- Create table for types of waste
 CREATE TABLE TipoResiduo (
     id_tipo_residuo INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(50) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
     toxico BOOLEAN NOT NULL,
     imagem MEDIUMBLOB DEFAULT NULL
 );
 
+-- Create table for users
 CREATE TABLE Usuario (
     id_usu INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(100) NOT NULL,
@@ -52,6 +59,7 @@ CREATE TABLE Usuario (
     CONSTRAINT check_cnpj_cpf_digits CHECK(cpf_cnpj REGEXP '^[0-9]{9,14}$')
 );
 
+-- Create table for user types
 CREATE TABLE TipoUsuario (
     id_tipo_usu INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome_tipo VARCHAR(50) UNIQUE NOT NULL,
@@ -59,12 +67,14 @@ CREATE TABLE TipoUsuario (
     CONSTRAINT check_level_range CHECK(nivel >= 1 AND nivel <=3)
 );
 
+-- Create table to establish a many-to-many relationship between waste types and disposal locations
 CREATE TABLE TipoResiduo_LocalDescarte (
     fk_TipoResiduo_id INT,
     fk_LocalDescarte_id INT,
     PRIMARY KEY (fk_TipoResiduo_id, fk_LocalDescarte_id)
 );
 
+-- Add foreign key constraints
 ALTER TABLE LocalDescarte ADD CONSTRAINT FK_LocalDescarte_Empresa 
 FOREIGN KEY (fk_Empresa_id) 
 REFERENCES Empresa(id_empresa) 
